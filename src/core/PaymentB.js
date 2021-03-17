e2345ec497781aae8c20c0844864f3da56e69a54
Ismael <isml_24@hotmail.com>
2021-03-17 14:15:10 +0100

    init

---
+++ src/core/PaymentB.js
@@ -0,0 +1,142 @@
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import { cartEmpty } from "./helper/CartHelper";
+import { getmeToken, processPayment } from "./helper/PaymentHelper";
+import { createOrder } from "./helper/OrderHelper";
+import { isAuthenticated, signout } from "../auth/helper";
+
+import DropIn from "braintree-web-drop-in-react";
+
+const PaymentB = ({ products, reload = undefined, setReload = (f) => f }) => {
+   const [info, setInfo] = useState({
+      loading: false,
+      success: false,
+      clientToken: null,
+      error: "",
+      instance: {},
+   });
+   const userId = isAuthenticated && isAuthenticated().user.id;
+   const token = isAuthenticated && isAuthenticated().token;
+   const getToken = (userId, token) => {
+      getmeToken(userId, token).then((info) => {
+         if (info.error) {
+            setInfo({
+               ...info,
+               error: info.error,
+            });
+            console.log("This is the error", info.error);
+            signout(() => {
+               return <Redirect to="/" />;
+            });
+         } else {
+            const clientToken = info.clientToken;
+            setInfo({ clientToken });
+         }
+      });
+   };
+
+   useEffect(() => {
+      getToken(userId, token);
+   }, []);
+
+   const getAmount = () => {
+      let amount = 0;
+      products.map((p) => {
+         amount = amount + parseInt(p.price);
+      });
+      return amount;
+   };
+
+   const onPurchase = () => {
+      setInfo({ loading: true });
+      let nonce;
+      let getNonce = info.instance
+         .requestPaymentMethod()
+         .then((data) => {
+            nonce = data.nonce;
+            const paymentData = {
+               paymentMethodNonce: nonce,
+               amount: getAmount(),
+            };
+            processPayment(userId, token, paymentData)
+               .then((response) => {
+                  if (response.error) {
+                     if (response.code == "1") {
+                        console.log("PAYMENT FAILED");
+                        signout(() => {
+                           return <Redirect to="/" />;
+                        });
+                     }
+                  } else {
+                     setInfo({ ...info, success: response.success, loading: false });
+                     console.log("PAYMENT SUCCESS");
+                     let product_names = "";
+                     products.forEach(function (item) {
+                        product_names += item.name + ", ";
+                     });
+
+                     const orderData = {
+                        products: product_names,
+                        transaction_id: response.transaction.id,
+                        amount: response.transaction.amount,
+                     };
+
+                     createOrder(userId, token, orderData)
+                        .then((response) => {
+                           if (response.error) {
+                              if (response.code == "1") {
+                                 console.log("ORDER FAILED");
+                              }
+                              signout(() => {
+                                 return <Redirect to="/" />;
+                              });
+                           } else {
+                              if (response.success == true) {
+                                 console.log("ORDER PLACED");
+                              }
+                           }
+                        })
+                        .catch((error) => {
+                           setInfo({ loading: false, success: false });
+                           console.log("ORDER FAILED", error);
+                        });
+
+                     cartEmpty(() => {
+                        console.log("CART IS EMPTY OUT");
+                     });
+                     setReload(!reload);
+                  }
+               })
+               .catch((e) => console.log(e));
+         })
+         .catch((e) => console.log("NONCE", e));
+   };
+
+   const ShowDropIn = () => {
+      return (
+         <div>
+            {info.clientToken !== null && products.length > 0 ? (
+               <div>
+                  <DropIn
+                     options={{ authorization: info.clientToken }}
+                     onInstance={(instance) => (info.instance = instance)}
+                  ></DropIn>
+                  <button onClick={onPurchase} className="btn btn-block btn-success">
+                     BUY
+                  </button>
+               </div>
+            ) : (
+               <h3>Please login frist or order something1 </h3>
+            )}
+         </div>
+      );
+   };
+   return (
+      <div>
+         <h3>Your bill is {getAmount()} NO DENN</h3>
+         {ShowDropIn()}
+      </div>
+   );
+};
+
+export default PaymentB;
